b2aba069f5a4abb3eec13b55b0a841212ca6eb65
1085069832 <1085069832@qq.com>
2017-07-06 16:48:17 +0800

    导入有错误

--- Assets/LeapMotionVR/Scripts/Editor/LeapTemporalWarpingEditor.cs
+++ Assets/LeapMotionVR/Scripts/Editor/LeapTemporalWarpingEditor.cs
@@ -1,29 +1,36 @@
-﻿using UnityEngine;
+/******************************************************************************
+ * Copyright (C) Leap Motion, Inc. 2011-2017.                                 *
+ * Leap Motion proprietary and  confidential.                                 *
+ *                                                                            *
+ * Use subject to the terms of the Leap Motion SDK Agreement available at     *
+ * https://developer.leapmotion.com/sdk_agreement, or another agreement       *
+ * between Leap Motion and you, your company or other organization.           *
+ ******************************************************************************/
+
 using UnityEditor;
-using System.Collections;
-using System.Collections.Generic;
 
-[CustomEditor(typeof(LeapVRTemporalWarping))]
-public class LeapTemporalWarpingEditor : Editor {
+namespace Leap.Unity {
 
-  private List<string> BasicModePropertyNames = new List<string>() {
-    "m_Script",
-    "recenter",
-  };
+  [CustomEditor(typeof(LeapVRTemporalWarping))]
+  public class LeapTemporalWarpingEditor : CustomEditorBase {
 
-  public override void OnInspectorGUI() {
-    serializedObject.Update();
-    SerializedProperty properties = serializedObject.GetIterator();
+    protected override void OnEnable() {
+      base.OnEnable();
 
-    bool useEnterChildren = true;
-    while (properties.NextVisible(useEnterChildren) == true) {
-      useEnterChildren = false;
-      if (AdvancedMode._advancedModeEnabled || BasicModePropertyNames.Contains(properties.name)) {
-        EditorGUILayout.PropertyField(properties, true);
+      specifyConditionalDrawing("allowManualTimeAlignment",
+                                "warpingAdjustment",
+                                "unlockHold",
+                                "moreRewind",
+                                "lessRewind");
+
+      specifyCustomDecorator("provider", warningDecorator);
+    }
+
+    private void warningDecorator(SerializedProperty prop) {
+      if (!PlayerSettings.virtualRealitySupported) {
+        EditorGUILayout.HelpBox("Unity VR Disabled.  ManualyUpdateTemporalWarping must be called right after " +
+                                "the Head transform has been updated.", MessageType.Warning);
       }
     }
-    serializedObject.ApplyModifiedProperties();
   }
 }
-
-  
